9a06b452444623ac91e6127cf8c0a8ddc3580bef
NailulFithriya <nailulfithriya2510@gmail.com>
2020-12-13 08:12:42 +0700

    first commit

---
+++ BarisanAritmetika.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class BarisanAritmetika here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BarisanAritmetika extends Barisan
+{
+    // instance variables - replace the example below with your own
+    public int kenaikan;
+
+    /**
+     * Constructor for objects of class BarisanAritmetika
+     */
+    public BarisanAritmetika(int nilaiAwal, int kenaikan)
+    {
+        super(nilaiAwal);
+        this.kenaikan = kenaikan;
+    }
+
+    @Override
+    public void naik()
+    {
+        super.nilaiBerikutnya += kenaikan;
+    }
+}
